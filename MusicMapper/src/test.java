480db4b53fcfaa0cc113c647ccf2e489802f2002
Michael Rehani <90718054+mrrehani@users.noreply.github.com>
2022-07-17 21:14:53 -0500

    Add files via upload

---
+++ MusicMapper/src/test.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println( System.getProperty("user.dir")+File.separator+"data" + File.separator + "metal_bands_2017_updated.csv");
+		System.out.println();
+	}
+}
